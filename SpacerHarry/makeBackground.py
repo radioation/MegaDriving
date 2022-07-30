8d72513230c433a08b660cc7cff8c36f9b3f65a3
Greg Gallardo <greggallardo@gmail.com>
2022-07-29 19:11:23 -0500

    working on background generation script

--- SpacerHarry/makeBackground.py
+++ SpacerHarry/makeBackground.py
@@ -9,17 +9,17 @@ bottomLeftStart =  ( width - bottomStripWidth * width ) / 2;
 height = 224
 step = 32
 
-img = Image.new( mode="RGB", size = (width,height), color = "darkgreen" )
+img = Image.new( mode="RGB", size = (width,height), color = (85,146,42) )
 dImage = ImageDraw.Draw( img )
 
 
 for x in range( 0, width,2 ):
-    shape = [ (x, 0), ( bottomLeftStart + x*bottomStripWidth, 223 ), (bottomLeftStart +  x*bottomStripWidth + bottomStripWidth, 223), (x,0 ) ]
-    dImage.polygon( shape, fill = "green" , outline="green")
+  shape = [ (x, 0), ( bottomLeftStart + x*bottomStripWidth, 223 ), (bottomLeftStart +  x*bottomStripWidth + bottomStripWidth, 223), (x,0 ) ]
+  dImage.polygon( shape, fill = (183,182,80) , outline=(183,182,80))
 
 
 
-img.save("test.png")
+img.save("starter.png")
 
 
 
