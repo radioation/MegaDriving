865ac4849d22dfe754dd76dfd21986aeca9cc40e
Greg Gallardo <greggallardo@gmail.com>
2023-02-18 15:05:31 -0600

    tweaking tile values

--- spacer/SpacerHarryCC/makeBackgroundCC.py
+++ spacer/SpacerHarryCC/makeBackgroundCC.py
@@ -5,10 +5,10 @@ import math
 
 width = 512
 midpint = width / 2
-bottomStripWidth = 48
+buttomTileWidth = 48
 height = 400   # try differnt values to get slightly different tile results.  I like 400~ish the best so far.
 
-bottomLeftStart =  ( width - bottomStripWidth * width ) / 2;
+bottomLeftStart =  ( width - buttomTileWidth * width ) / 2;
 
 img = Image.new( mode="P", size = (width,height))
 img.putpalette([
@@ -36,11 +36,11 @@ worldY =  -500
 
 
 start = 0  # bottom based on Lou's page.
-end = 160  # up
+end = 159  # up
 lowZ = worldY/ (start - height/2)
 highZ = worldY/ (end - height/2)
 
-tiles = 27 
+tiles = 25 
 tileZ = ( highZ - lowZ ) / tiles
 
 lastZ = lowZ
@@ -70,10 +70,11 @@ for y in tileYs:
 # draw from bottom up
 currY = height - 1
 # Tile width variables
-bottomStripWidth = 48
+buttomTileWidth = 48
 topStripWidth = 1
-widthDelta = (bottomStripWidth - topStripWidth) / (end - start )
-currWidth = bottomStripWidth
+tileWidthDelta = (buttomTileWidth - topStripWidth) / (end - start )
+print(tileWidthDelta)
+currTileWidth = buttomTileWidth
 
 # tile depth variables
 tiles2 = tiles * 4 
@@ -84,7 +85,7 @@ color1 = 1
 color2 = 5
 for endY in tileYs:
   # how many X iterations?
-  xCount = math.ceil(width/(2* currWidth)) +1
+  xCount = math.ceil(width/(2* currTileWidth)) +1
   colorOffset = 0
   for y in range ( currY, endY, -1 ) :
     # color choice now depends on a different distance
@@ -96,8 +97,8 @@ for endY in tileYs:
         if z2 >= z :
           colorOffset+=1
           lastZ += tileZ2
-        #if ( colorOffset > 3 ) :
-        #  colorOffset = 3;
+        if ( colorOffset > 3 ) :
+          colorOffset = 3;
     elif  currY - endY > 2 :
       # use 3 of 4 possible color offset
       if y < currY-1:
@@ -107,16 +108,16 @@ for endY in tileYs:
       colorOffset = 0
 
     # spread out from center.
-    p1 = int(width/2 -1)
-    p2 = int(p1 - currWidth)
+    p1 = round(width/2 -1)
+    p2 = round(p1 - currTileWidth)
     for x in range( 0, xCount ) :
       dImage.line( [(p1,y) , (p2,y)], fill =  color1 + colorOffset if x%2 == 0 else color2 + colorOffset )
       # and opposites
       dImage.line( [(511-p1,y) , (512-p2,y)], fill =  color2 + colorOffset if x%2 == 0 else color1 + colorOffset )
       p1 = p2 -1
-      p2 = p1 - currWidth
+      p2 = p1 - currTileWidth
 
-    currWidth -= widthDelta
+    currTileWidth -= tileWidthDelta
 
   # handle next tile
   currY = endY
